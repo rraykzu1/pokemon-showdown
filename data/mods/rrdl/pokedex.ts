cc0049235e4c6ce9883b9ec4caf3c65c16eb749a
Karthik99999 <bandagondak0217@gmail.com>
2021-04-12 11:16:42 -0400

    add luninja

--- data/mods/rrdl/pokedex.ts
+++ data/mods/rrdl/pokedex.ts
@@ -2205,7 +2205,7 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 
 	// custom mons
 	tyrogunk: {
-		num: -1000,
+		num: -10000,
 		name: "Tyrogunk",
 		types: ["Poison", "Fighting"],
 		baseStats: {hp: 83, atk: 96, def: 75, spa: 96, spd: 75, spe: 85},
@@ -2213,10 +2213,10 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 		heightm: 0.7,
 		weightkg: 22,
 		color: "Purple",
-		eggGroups: ["Undiscovered"],
+		eggGroups: ["Undiscovered"]
 	},
 	clefpex: {
-		num: -1001,
+		num: -10001,
 		name: "Clefpex",
 		types: ["Poison", "Fairy"],
 		baseStats: {hp: 85, atk: 50, def: 100, spa: 80, spd: 105, spe: 80},
@@ -2224,6 +2224,17 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 		heightm: 2,
 		weightkg: 54.5,
 		color: "Pink",
-		eggGroups: ["Undiscovered"],
+		eggGroups: ["Undiscovered"]
+	},
+	luninja: {
+		num: -10002,
+		name: "Luninja",
+		types: ["Fighting", "Water"],
+		baseStats: {hp: 72, atk: 85, def: 67, spa: 95, spd: 71, spe: 110},
+		abilities: {0: "Adaptability", 1: "Prankster", H: "Technician"},
+		heightm: 1.35,
+		weightkg: 47,
+		color: "Blue",
+		eggGroups: ["Undiscovered"]
 	},
 }
