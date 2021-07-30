fe4cd8dca93486d1de19a6fd05f1b6ce682b54d7
Karthik99999 <bandagondak0217@gmail.com>
2021-07-30 20:28:33 +0530

    Move Arceus Forme Clause to mod data

---
+++ data/mods/radred/rulesets.ts
@@ -0,0 +1,8 @@
+export const Rulesets: {[k: string]: ModdedFormatData} = {
+	arceusformeclause: {
+		name: "Arceus Forme Clause",
+		effectType: "ValidatorRule",
+		desc: "Allows Arceus plates to be used in battle.",
+		unbanlist: ['Flame Plate', 'Splash Plate', 'Zap Plate', 'Meadow Plate', 'Icicle Plate', 'Fist Plate', 'Toxic Plate', 'Earth Plate', 'Sky Plate', 'Mind Plate', 'Insect Plate', 'Stone Plate', 'Spooky Plate', 'Draco Plate', 'Dread Plate', 'Iron Plate', 'Pixie Plate'],
+	},
+}
