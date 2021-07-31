e8c25f267bcbc6d9f7d6e9f7d1b8ed161718c792
pacmanboss256 <pacmanboss972@gmail.com>
2021-07-31 17:22:34 -0500

    fix eslint

--- data/mods/radred/rulesets.ts
+++ data/mods/radred/rulesets.ts
@@ -5,4 +5,4 @@ export const Rulesets: {[k: string]: ModdedFormatData} = {
 		desc: "Allows Arceus plates to be used in battle.",
 		unbanlist: ['Flame Plate', 'Splash Plate', 'Zap Plate', 'Meadow Plate', 'Icicle Plate', 'Fist Plate', 'Toxic Plate', 'Earth Plate', 'Sky Plate', 'Mind Plate', 'Insect Plate', 'Stone Plate', 'Spooky Plate', 'Draco Plate', 'Dread Plate', 'Iron Plate', 'Pixie Plate'],
 	},
-}
+};
