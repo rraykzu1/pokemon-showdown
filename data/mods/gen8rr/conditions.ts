3ad7366e2452ff20b5146569f85f3855a726b02f
Karthik99999 <bandagondak0217@gmail.com>
2022-08-06 16:48:00 -0400

    Fix visual bug with recharge moves

--- data/mods/gen8rr/conditions.ts
+++ data/mods/gen8rr/conditions.ts
@@ -47,9 +47,12 @@ export const Conditions: {[k: string]: ModdedConditionData} = {
 			}
 		}
 	},
+	mustrecharge: {
+		inherit: true,
+		onStart() {},
+	},
 	gem: {
-		duration: 1,
-		affectsFainted: true,
+		inherit: true,
 		onBasePower(basePower, user, target, move) {
 			this.debug('Gem Boost');
 			return this.chainModify(1.5);
